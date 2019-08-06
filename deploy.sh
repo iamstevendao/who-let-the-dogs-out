9e555f75b923afe47a730354cf123aeb9da4eb43
Steven Dao <iamstevendao@gmail.com>
2019-08-07 00:10:57 +1000

    Remove github CNAME to deploy to Netlify

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-echo 'www.who-let-the-dogs-out.iamstevendao.com' > CNAME
+# echo 'www.who-let-the-dogs-out.iamstevendao.com' > CNAME
 
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
