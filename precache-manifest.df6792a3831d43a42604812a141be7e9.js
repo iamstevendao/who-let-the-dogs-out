4c04b1951a966a88b165819b872123d42e617481
Steven Dao <iamstevendao@gmail.com>
2021-01-05 12:49:39 +0000

    Deploy

---
+++ precache-manifest.df6792a3831d43a42604812a141be7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0ac2ebe98fa5f3cb15c3",
+    "url": "/js/chunk-vendors.61010e9a.js"
+  },
+  {
+    "revision": "c76fdcc67466a1bffe91",
+    "url": "/js/app.376fe287.js"
+  },
+  {
+    "revision": "2f8a9ef468467cd929824a06d23a4cf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "308a31668d508547d14ba1762fa1566b",
+    "url": "/circle.yml"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.df6792a3831d43a42604812a141be7e9.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "who-let-the-dogs-out"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
